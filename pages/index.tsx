36e5d1d962455a2af3ed9f76ce10230240dca00d
hadara <vlopes45@gmail.com>
2023-06-30 10:03:59 -0400

    Improve SEO and fetch image with correct size from github profile

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope, faSheetPlastic } from '@fortawesome/free-solid-svg-icons'
 import Image from 'next/image'
+import Head from 'next/head'
 
 const links = [
   {
@@ -21,19 +22,24 @@ const links = [
   }
 ]
 
-function profilePictureLoader({ src, width, quality }: {src: string, width: number, quality?: number | undefined}) {
-  return `https://github.com/${src}.png?size=${width}`
+function profilePictureLoader({ src, width}: {src: string, width: number, quality?: number | undefined}) {
+  return `${src}.png?size=${width}`
 }
 
 export default function Home() {
   return (
     <main className="bg-navy-blue max-h-screen h-screen overflow-x-hidden flex flex-col md:flex-row">
+       <Head >
+        <title>Vinícius Lopes | Software Developer</title>
+        <meta charSet="utf-8" />
+        <meta name="description" content="Vinícius Lopes | Software Developer | Portifolio" />
+      </Head>
       <div className="flex flex-row md:flex-col items-center p-3 border-b-2 md:border-b-0 md:border-r-2 border-yellow-300 h-[64px] md:min-h-full md:h-full">
-        <Image loader={profilePictureLoader} src={me.photo} width={64} height={64} alt="me" className="rounded-full w-[32px] md:w-[64px] mr-8 md:mr-0 md:mb-8" />
-        <Link className="mr-8 md:mr-0 md:mb-8" target='_blank' href="https://github.com/uasouz" ><FontAwesomeIcon size="2x" className="text-yellow-300 hover:text-gray-300 text-xl md:text-3xl" icon={faGithub} /></Link>
-        <Link className="mr-8 md:mr-0 md:mb-8" target='_blank' href="https://linkedin.com/in/vlopes45" ><FontAwesomeIcon size="2x" className="text-yellow-300 hover:text-gray-300 text-xl md:text-3xl" icon={faLinkedinIn} /></Link>
-        <Link className="mr-8 md:mr-0 md:mb-8" target='_blank' href="mailto:vlopes45@gmail.com" ><FontAwesomeIcon size="2x" className="text-yellow-300 hover:text-gray-300 text-xl md:text-3xl" icon={faEnvelope} /></Link>
-        <Link className='border-2 rounded border-yellow-300 p-2 text-yellow-300 hover:text-gray-300' target='_blank' href=''><FontAwesomeIcon size="2x" className="text-yellow-300 hover:text-gray-300 text-xl md:text-3xl" icon={faSheetPlastic} /></Link>
+        <Image loader={profilePictureLoader} src={`${me.photo}?size=64`} width={64} height={64} alt="me" className="rounded-full w-[32px] md:w-[64px] mr-8 md:mr-0 md:mb-8" />
+        <Link aria-label={"Github link"} className="p-2 mr-8 md:mr-0 md:mb-8" target='_blank' href="https://github.com/uasouz" ><FontAwesomeIcon size="2x" className="text-yellow-300 hover:text-gray-300 text-xl md:text-3xl" icon={faGithub} /></Link>
+        <Link aria-label={"Linkedin link"} className="p-2 mr-8 md:mr-0 md:mb-8" target='_blank' href="https://linkedin.com/in/vlopes45" ><FontAwesomeIcon size="2x" className="text-yellow-300 hover:text-gray-300 text-xl md:text-3xl" icon={faLinkedinIn} /></Link>
+        <Link aria-label='Mail to' className="p-2 mr-8 md:mr-0 md:mb-8" target='_blank' href="mailto:vlopes45@gmail.com" ><FontAwesomeIcon size="2x" className="text-yellow-300 hover:text-gray-300 text-xl md:text-3xl" icon={faEnvelope} /></Link>
+        <Link aria-label="Resume Download" className='border-2 rounded border-yellow-300 p-2 text-yellow-300 hover:text-gray-300' target='_blank' href=''><FontAwesomeIcon size="2x" className="text-yellow-300 hover:text-gray-300 text-xl md:text-3xl" icon={faSheetPlastic} /></Link>
       </div>
       <div className="relative flex flex-col w-screen mx-auto font-sans text-base max-w-1440 text-dawn lg:flex-row">
 
